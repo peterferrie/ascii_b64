5fd225b0cf8d53467a77eea582577b34a9951344
peterferrie <peter.ferrie@gmail.com>
2014-11-08 15:41:50 -0800

    reduce decoder size

--- asciib64.asm
+++ asciib64.asm
@@ -19,18 +19,14 @@ _decoder:
 %endif
         sub     [esi + 09h + b64decode - _decoder], bx
         sub     [esi + 0fh + b64decode - _decoder], ebx
-        sub     [esi + 11h + b64decode - _decoder], bl
-        sub     [esi + 13h + b64decode - _decoder], ebx
-        sub     [esi + 1ah + b64decode - _decoder], bl
+        sub     [esi + 18h + b64decode - _decoder], bx
         sub     [esi + 1eh + b64decode - _decoder], bl
         sub     [esi + 20h + b64decode - _decoder], bl
-        sub     [esi + 22h + b64decode - _decoder], ebx
-        sub     [esi + 24h + b64decode - _decoder], bl
+        sub     [esi + 23h + b64decode - _decoder], ebx
+        sub     [esi + 24h + b64decode - _decoder], bx
         sub     [esi + 27h + b64decode - _decoder], ebx
-        sub     [esi + 29h + b64decode - _decoder], bl
-        sub     [esi + 2bh + b64decode - _decoder], bl
-        sub     [esi + 2ch + b64decode - _decoder], bx
-        sub     [esi + 2ch + b64decode - _decoder], bx
+        sub     [esi + 2bh + b64decode - _decoder], ebx
+        sub     [esi + 2bh + b64decode - _decoder], ebx
 
 b64decode:
         ;add    esi, b64decode_end - _decoder
@@ -57,58 +53,51 @@ b64_inner:
 ;09
         db      (0c1h + key1) & 0ffh, (0c0h + 1 + key2) & 0ffh, 28h
         cmp     al, '0'
-        ;jnb    b64_testchar
+        ;jnb    b64_testupr
 ;0f
         db      73h, (05h + key1) & 0ffh
-        ;add    al, (('/' shl 2) + 1) & 0ffh
-;11
-        db      (04h + key2) & 0ffh, (0bdh + key3 + key1) & 0ffh
         ;shr    al, 2 ;because '+' and '/' differ by only 1 bit
-;12, 13
-        db      (0c0h + key4) & 0ffh, (0e8h + key1) & 0ffh, (02h + 1 + key2) & 0ffh
-
-b64_testchar:
-        ;add    al, 4
-        db      (04h + key3) & 0ffh, (04h + key4) & 0ffh
-        cmp     al, 3fh
-        ;jbe    b64_store
-;1a
-        db      76h, (08h + key1) & 0ffh
-        sub     al, 45h
-        ;cmp    al, 19h
+        db      (0c0h + key2) & 0ffh, (0e8h + 1 + key3) & 0ffh, (02h + 1 + key4) & 0ffh
+        xor     al, '0' ;concatenate numbers and '+' and '/'
+b64_testupr:
+        cmp     al, 'A'
+        ;jnb    b64_testlwr
+;18
+        db      73h, (02h + key1) & 0ffh
+        ;add    al, ('z' + 1) - '0' ;concatenate lowercase and numbers
+        db      (04h + key1) & 0ffh, ('z' + 1) - '0'
+b64_testlwr:
+        cmp     al, 'a'
 ;1e
-        db      3Ch, (19h + key1) & 0ffh
-        ;jbe    b64_store
+        ;jb     b64_store
+        db      72h, (02h + key1) & 0ffh
+        ;sub    al, 6 ;concatenate uppercase and lowercase
 ;20
-        db      76h, (02h + key1) & 0ffh
-        ;sub    al, 6
-;22
         db      2Ch, (06h + key1) & 0ffh
-
 b64_store:
-        ;shrd   ebx, eax, 26h
-        ;again, we want 6, CPU performs &1fh, so we can encode as ASCII to avoid decoding
-;24
-        db      (0fh + key2) & 0ffh, (0ach + key3 + key1) & 0ffh, (0c3h + key4) & 0ffh, 26h
+        sub     al, 'A'
+        ;shrd   ebx, eax, 6
+;23, 24
+        db      (0fh + key1) & 0ffh, (0ach + key1 + key2) & 0ffh, (0c3h + 1 + key2 + key3) & 0ffh, (06h + key4) & 0ffh
         ;loop   b64_inner
 ;27
         db      (0e2h + key1) & 0ffh, (0e0h + 1 + key2) & 0ffh
-        ;xchg   ebx, eax
-;29
-        db      (93h + 1 + key3 + key1) & 0ffh
         ;bswap  eax
+        db      (0fh + 1 + key3) & 0ffh, (0cbh + key4) & 0ffh
 ;2b
-        db      (0fh + 1 + key4) & 0ffh, (0c8h + key1) & 0ffh
+        ;nop ;alignment
+        db      (90h + key1 + key1) & 0ffh
+        ;xchg   ebx, eax
+        db      (93h + 1 + key2 + key2) & 0ffh
         ;stosd
-;2c
-        db      (0abh + key1 + key1) & 0ffh
+        db      (0abh + 1 + key3 + key3) & 0ffh
         ;cmp    byte [esi], '+'
-        db      (80h + 1 + key2 + key2) & 0ffh, 3eh, 2bh
+        db      (80h + 1 + key4 + key4) & 0ffh, 3eh, 2bh
         ;[dec   edi]
         ;[jnb   b64_outer]
         ;the dec and branch are base64-encoded to reduce size
 
 b64decode_end:
-        db      "T3PS" 
+        db      "T3PR" 
         ;append your base64 data here
         ;terminate with printable character less than '+'
